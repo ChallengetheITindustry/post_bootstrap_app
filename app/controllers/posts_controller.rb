e04492003ff81bcdc872c69ac3d4f5f5adfbe62e
ohnishi taisei <torasora@ohnishitaiseiMacBook-Pro.local>
2020-12-06 17:06:52 +0900

    投稿ページを部分テンプレートで共通化

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -31,13 +31,12 @@ class PostsController < ApplicationController
     redirect_to root_path
   end
 
-  
   private
 
   def set_post
     @post = Post.find(params[:id])
   end
-  
+
   def post_params
     params.require(:post).permit(:title, :content)
   end
